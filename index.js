e7829e1dc28c08b9fdecfa01ea46609331bf88cb
Michael Manders <MJ@Michaels-MacBook-Pro.local>
2022-01-24 21:24:53 -0600

    heroku deployment fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-const hostname = '127.0.0.1'
+const hostname = process.env.HOSTNAME || '127.0.0.1'
 const port = process.env.PORT || 5000; // define port 
 
 const server = http.createServer(async (req, res) => {
